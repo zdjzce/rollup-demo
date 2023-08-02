2e73ba37a096ce8100b83ba3cd04945c0805358e
zdjzce923 <493904954@qq.com>
2023-08-02 22:15:05 +0800

    feat: build watch api

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,10 @@
  * @type { import('rollup').RollupOptions }
  */
 
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
 /* 多入口多出口多产物 */
 const buildOptions = {
   input: ["src/index.js", "src/util.js"],
@@ -17,25 +21,27 @@ const buildOptions = {
     {
       dir: "dist/cjs",
       format: "cjs"
-    }
-  ]
+    },
+  ],
+  plugin: [resolve(), commonjs(), terser()]
 
 };
 
 // 不同入口对应的打包配置不一样：
-const secondOptions = {
-  input: ["src/index.js"],
-  output: [
-    {
-      dir: "dist/second/esm",
-      format: "esm"
-    },
-    {
-      dir: "dist/second/cjs",
-      format: "cjs"
-    }
-  ]
-}
+// const secondOptions = {
+//   input: ["src/index.js"],
+//   output: [
+//     {
+//       dir: "dist/second/esm",
+//       format: "esm"
+//     },
+//     {
+//       dir: "dist/second/cjs",
+//       format: "cjs"
+//     }
+//   ],
+//   plugin: [resolve(),]
+// }
 
 
-export default [buildOptions, secondOptions];
+export default [buildOptions];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // src/index.js
+
+import { cloneDeep } from 'lodash/cloneDeep'
 import { add } from "./util";
-console.log(add(1, 2));
+console.log(add(1, 2, 3));
+
+const clone = cloneDeep({test: 1})
+console.log('clone:', clone)
